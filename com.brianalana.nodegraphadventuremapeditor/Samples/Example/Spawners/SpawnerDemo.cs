718e3edca0661c930341492438940a63985cbdd3
Briana <brea83@gmail.com>
2026-05-06 17:19:09 +0200

    fixing spawnerDemo to be compatible with the graph

--- com.brianalana.nodegraphadventuremapeditor/Samples/Example/Spawners/SpawnerDemo.cs
+++ com.brianalana.nodegraphadventuremapeditor/Samples/Example/Spawners/SpawnerDemo.cs
@@ -1,10 +1,12 @@
 using UnityEngine;
 using NGAME;
+using System.Collections.Generic;
 
-public class SpawnerDemo : MonoBehaviour
+public class SpawnerDemo : MonoBehaviour, ISpawnPoint
 {
     [SerializeField]
-    private SpawnerData m_SpawnData = new();
+    private List<SO_SpawnTypeTag> m_AllowedSpawnTypes;
+    public List<SO_SpawnTypeTag> AllowedSpawnableTypes { get => m_AllowedSpawnTypes; set => m_AllowedSpawnTypes = value; }
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -16,5 +18,17 @@ public class SpawnerDemo : MonoBehaviour
     void Update()
     {
         
+    }
+
+
+    public Vector3 GetPosition()
+    {
+        return transform.position;
+    }
+
+    public SpawnerData GetSpawnerData()
+    {
+        SpawnerData result = new SpawnerData() { Name = name, Position = transform.position, ValidTypes = m_AllowedSpawnTypes };
+        return result;
     }
 }
